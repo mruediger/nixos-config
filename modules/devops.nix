9da53efefd4590a65e0c4e9117422297d0375419
Mathias Rüdiger <ruediger@blueboot.org>
2019-01-17 10:25:39 +0100

    update terraform and google cloud sdk and add kubectl

--- modules/devops.nix
+++ modules/devops.nix
@@ -1,10 +1,29 @@
-{ pkgs, ... }:
-{
+{ pkgs, config, ... }:
+
+let
+  unstableTarball =
+    fetchTarball
+      https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz;
+in {
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      unstable = import unstableTarball {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     git
-    terraform
     aws
-    google-cloud-sdk
-#    aws-mfa
+    unstable.terraform
+    unstable.google-cloud-sdk
+    unstable.kubectl
+    #    aws-mfa
   ];
+
+  programs.bash.shellAliases = {
+    k = "kubectl";
+  };
+
 }
