2e4deaf8f44f123e8d474e31f7417c8dd83afcf1
Mathias Rüdiger <ruediger@blueboot.org>
2019-10-01 13:52:45 +0200

    cleanup emacs config

--- desktop/emacs.nix
+++ desktop/emacs.nix
@@ -1,10 +1,4 @@
 { pkgs, lib, config, ... }:
-let
-  wrapped = pkgs.writeScriptBin "emacs" ''
-    #!${pkgs.stdenv.shell}
-    exec ${pkgs.emacs}/bin/emacs --eval "(setq user-emacs-directory ${toString ./.})"
-  '';
-in
 {
 
   environment.systemPackages = with pkgs; [
