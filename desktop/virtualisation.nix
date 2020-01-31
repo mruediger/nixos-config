37335ec5983163fe208c38280a28d31354cd61e2
Mathias Rüdiger <ruediger@blueboot.org>
2020-01-31 12:19:58 +0100

    add vagrant

--- desktop/virtualisation.nix
+++ desktop/virtualisation.nix
@@ -7,4 +7,17 @@
   users.extraUsers.bag.extraGroups = [ "libvirtd" ];
 
   environment.systemPackages = with pkgs; [ virtviewer virtmanager ];
+  environment.systemPackages = with pkgs; [
+    virtviewer
+    virtmanager
+    vagrant
+  ];
+
+  services.nfs.server.enable = true;
+
+  # for vagrant nfs
+  networking.firewall.extraCommands = ''
+    ip46tables -I INPUT 1 -i vboxnet+ -p tcp -m tcp --dport 2049 -j ACCEPT
+  '';
+
 }
