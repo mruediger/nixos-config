2dca1ac658bf00e81fdab42a21a2616134b28772
Mathias Rüdiger <ruediger@blueboot.org>
2020-08-13 11:04:05 +0200

    add gpg-agent settings for yubikey ssh

--- desktop/yubikey.nix
+++ desktop/yubikey.nix
@@ -14,6 +14,13 @@
   ];
   hardware.u2f.enable = true;
 
+environment.shellInit = ''
+  export GPG_TTY="$(tty)"
+  gpg-connect-agent /bye
+  export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+'';
+
+
   programs.ssh.startAgent = false;
   programs.gnupg.agent = {
     enable = true;
