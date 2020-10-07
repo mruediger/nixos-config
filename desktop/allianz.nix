dcb00b3b403daf246916ce6081ed760e0d83ad3a
Mathias Rüdiger <ruediger@blueboot.org>
2020-10-07 11:34:22 +0200

    add missing ]

--- desktop/allianz.nix
+++ desktop/allianz.nix
@@ -6,6 +6,7 @@ in
   environment.systemPackages = with pkgs; [
     citrix_workspace
     openconnect
+  ];
 
   security.pki.certificateFiles = [
     "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ../certificates/jira.gda.allianz
