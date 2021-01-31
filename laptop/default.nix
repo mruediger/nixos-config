b343375be8e4be391948ea3e76ebde7ac55a120f
Mathias Rüdiger <ruediger@blueboot.org>
2021-01-31 14:02:48 +0100

    remove hardware specific settings

--- laptop/default.nix
+++ laptop/default.nix
@@ -7,34 +7,7 @@
   environment.systemPackages = with pkgs; [
     powertop
     s-tui
+    brightnessctl
   ];
-
-  services.udev = {
-    extraRules = ''
-      SUBSYSTEM=="pci", ATTR{power/control}="auto"
-      SUBSYSTEM=="i2c", ATTR{power/control}="auto"
-      SUBSYSTEM=="net", ATTR{power/wakeup}="disabled"
-    '';
-  };
-
-  boot = {
-    kernel.sysctl."vm.dirty_writeback_centisecs" = 1500;
-    extraModprobeConfig = ''
-      options snd_hda_intel power_save=1
-    '';
-  };
-
-  services.tlp.enable = false;
-
-  services.upower.enable = true;
-
-  powerManagement = {
-    enable = true;
-    cpuFreqGovernor = "powersave";
-    powerUpCommands = ''
-      echo '1500' > '/proc/sys/vm/dirty_writeback_centisecs';
-    '';
-  };
-
 
 }
