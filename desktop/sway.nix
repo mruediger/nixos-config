e9dfaaee7e73b952e5321c205b16a1a77dc23727
Mathias Rüdiger <ruediger@blueboot.org>
2021-02-16 09:47:44 +0100

    change sway startup

--- desktop/sway.nix
+++ desktop/sway.nix
@@ -1,5 +1,20 @@
 # see https://github.com/colemickens/nixpkgs-wayland
-{ pkgs, ... }:
+{ pkgs, lib, unstable, ... } @ args:
+let
+  startsway = pkgs.writeTextFile {
+    name = "startsway";
+    destination = "/bin/startsway";
+    executable = true;
+    text = ''
+          #! ${pkgs.bash}/bin/bash
+
+          # first import environment variables from the login manager
+          systemctl --user import-environment
+          # then start the service
+          exec systemctl --user start sway.service
+          '';
+  };
+in
 {
 
   programs.sway = {
@@ -19,33 +34,43 @@
     ];
   };
 
-  users.extraUsers.bag.extraGroups = ["sway"];
+  programs.waybar.enable = true;
 
   environment.systemPackages = with pkgs; [
     fzf
     i3status
     unstable.firefox-wayland
     alacritty
+    xss-lock
+    startsway
   ];
 
-  services.xserver = {
-    enable = true;
-    displayManager = {
-      defaultSession = "sway";
-      gdm.enable = true;
-    };
-    libinput.enable = true;
+  systemd.user.targets.sway-session = {
+    description = "Sway compositor session";
+    documentation = [ "man:systemd.special(7)" ];
+    bindsTo = [ "graphical-session.target" ];
+    wants = [ "graphical-session-pre.target" ];
+    after = [ "graphical-session-pre.target" ];
   };
 
-  systemd.services.swaylock = {
-    enable = true;
-    before = [ "sleep.target" "suspend.target" ];
-    wantedBy = [ "sleep.target" "suspend.target" ];
+  systemd.user.services.sway = {
+    description = "Sway - Wayland window manager";
+    documentation = [ "man:sway(5)" ];
+    bindsTo = [ "graphical-session.target" ];
+    wants = [ "graphical-session-pre.target" ];
+    after = [ "graphical-session-pre.target" ];
+    # We explicitly unset PATH here, as we want it to be set by
+    # systemctl --user import-environment in startsway
+    environment.PATH = lib.mkForce null;
     serviceConfig = {
-      Type = "forking";
-      User = "bag";
-      Environment = "WAYLAND_DISPLAY=wayland-0 XDG_RUNTIME_DIR=/run/user/1000";
-      ExecStart = "${pkgs.swaylock}/bin/swaylock"; # -i ${toString ../.}/templates/w95lock.png";
+      Type = "simple";
+      ExecStart = ''
+        ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway --debug
+      '';
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
     };
   };
+
 }
