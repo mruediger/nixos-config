4c2b7442184215ccda4d1ae7c8995235e2e69ce3
Mathias Rüdiger <ruediger@blueboot.org>
2021-02-16 10:52:52 +0100

    configure openvpn for justwatch

--- desktop/justwatch.nix
+++ desktop/justwatch.nix
@@ -17,5 +17,16 @@
     kp="kubectl --context gke_justwatch-compute_europe-west1-d_jw-k8s-prod-eu-1";
   };
 
+  services.openvpn.servers = {
+    justwatch-gcp = {
+      config = '' config /home/bag/src/nixos/nixos-config/openvpn/jw-mathias.ruediger-gcp.ovpn '';
+      up = ''
+          ${pkgs.systemd}/bin/resolvectl dns $dev 10.132.10.40
+          ${pkgs.systemd}/bin/resolvectl domain $dev c.justwatch-compute.internal.
+          '';
+      down = "${pkgs.systemd}/bin/resolvectl revert $dev";
+    };
+  };
 
+  networking.extraHosts = "10.132.4.53 vault-2.justwatch.com";
 }
