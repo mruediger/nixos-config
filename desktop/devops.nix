87361e38f6be142f796a10775ec93fe2a89ff99c
Mathias Rüdiger <ruediger@blueboot.org>
2021-02-23 11:10:45 +0100

    some devops tools

--- desktop/devops.nix
+++ desktop/devops.nix
@@ -9,9 +9,11 @@
     google-cloud-sdk
     gnumake
     yq-go
+    bind
+    pwgen
     unstable.kops
     unstable.kubectl
-    unstable.terraform
+    unstable.terraform_0_14
     unstable.vault
     unstable.packer
   ];
