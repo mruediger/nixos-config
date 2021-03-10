2fa5255005c22bcd6caf846af32e8f1042c09ba3
Mathias Rüdiger <ruediger@blueboot.org>
2021-03-10 12:39:31 +0100

    add lsp dependencies

--- desktop/emacs.nix
+++ desktop/emacs.nix
@@ -18,6 +18,13 @@
     (rWrapper.override { packages = with rPackages; [ ggplot2 ]; })
 
     imagemagick # for image-dired
+
+    gopls # lsp go
+    rls   # rust lsp
+    terraform-ls
+    nodePackages.bash-language-server
+    nodePackages.yaml-language-server
+    rnix-lsp
   ];
 
 
