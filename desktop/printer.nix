c12d56d3279670d68c508da117fddc068bcc1970
Mathias Rüdiger <ruediger@blueboot.org>
2021-03-30 15:35:35 +0200

    updated printer settings

--- desktop/printer.nix
+++ desktop/printer.nix
@@ -1,9 +1,43 @@
 { pkgs, ... }:
 {
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    startWhenNeeded = true;
+    drivers = with pkgs; [ gutenprint hplip ];
+  };
 
   services.avahi = {
     enable = true;
     nssmdns = true;
   };
+
+  hardware.printers = {
+    ensurePrinters = [{
+      name = "HP_LaserJet_Pro_M148dw";
+      ppdOptions = {
+        PageSize = "A4";
+        Duplex = "DuplexNoTumble";
+      };
+      # lpinfo -v  or   hp-setup -i  or   hp-makeuri <IP>
+      deviceUri = "hp:/net/HP_LaserJet_Pro_M148-M149";
+
+      # lpinfo -m
+      model = "HP/hp-laserjet_pro_m148-m149-ps.ppd.gz";
+      description = "HP LaserJet Pro M148DW";
+      location = "Wohnzimmer";
+    }];
+    ensureDefaultPrinter = "HP_LaserJet_Pro_M148dw";
+  };
+
+  hardware.sane = {
+     enable = true;
+     extraBackends = with pkgs; [ sane-airscan ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    simple-scan
+    xsane
+  ];
+
+  users.extraUsers.bag.extraGroups = ["lp" "scanner"];
 }
