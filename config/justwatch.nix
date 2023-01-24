6377f8ef5ca47897e9ccddfca3dd8c23d8cb87f7
Mathias Rüdiger <ruediger@blueboot.org>
2023-01-24 09:51:06 +0100

    add terragrunt aliases

--- config/justwatch.nix
+++ config/justwatch.nix
@@ -15,11 +15,15 @@
   programs.bash.shellAliases = {
     ks="kubectl --context gke_justwatch-compute_europe-west1-b_jw-k8s-stage-eu-1";
     kp="kubectl --context gke_justwatch-compute_europe-west1-d_jw-k8s-prod-eu-1";
+    tgs="TF_VAR_env=stage terragrunt";
+    tgp="TF_VAR_env=prod terragrunt";
   };
 
   programs.bash.interactiveShellInit = ''
     source ${pkgs.complete-alias}/bin/complete_alias
     complete -F _complete_alias kp
     complete -F _complete_alias ks
+    complete -F _complete_alias tgs
+    complete -F _complete_alias tgp
   '';
 }
