f029484ea69f5587999b75bbe05541558ba358cc
Mathias Rüdiger <ruediger@blueboot.org>
2023-04-27 10:55:56 +0200

    slack wayland setup

--- home/sway.nix
+++ home/sway.nix
@@ -1,4 +1,16 @@
 { pkgs, config, lib, ... }:
+let
+  slack = pkgs.slack.overrideAttrs (old: {
+    installPhase = old.installPhase + ''
+      rm $out/bin/slack
+
+      makeWrapper $out/lib/slack/slack $out/bin/slack \
+        --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+        --prefix PATH : ${lib.makeBinPath [pkgs.xdg-utils]} \
+        --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
+    '';
+  });
+in
 {
   home.packages = with pkgs; [
     unstable.google-chrome
