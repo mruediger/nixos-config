a04a290785c11eaf88e86b7fa5e11e0e3cdb270d
Mathias Rüdiger <ruediger@blueboot.org>
2023-10-04 11:54:56 +0200

    enable pulseaudio via tcp

--- config/audio.nix
+++ config/audio.nix
@@ -3,9 +3,18 @@
   environment.systemPackages = with pkgs; [
     pavucontrol
     helvum
+    pulseaudio
   ];
 
   security.rtkit.enable = true;
+
+  services.avahi = {
+    enable = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+  };
 
   services.pipewire = {
     enable = true;
@@ -13,14 +22,40 @@
     pulse.enable = true;
   };
 
-  environment.etc = {
-	  "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-		bluez_monitor.properties = {
-			["bluez5.enable-sbc-xq"] = true,
-			["bluez5.enable-msbc"] = true,
-			["bluez5.enable-hw-volume"] = true,
-			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-		 }
-	'';
-  };
+  environment.etc =
+    let
+      json = pkgs.formats.json { };
+    in
+    {
+      "pipewire/pipewire-pulse.conf.d/99-network.conf".source = json.generate "99-network.conf"
+        {
+          "pulse.properties" = {
+            "server.address" = [
+              "unix:native"
+              { address = "tcp:0.0.0.0:4713"; "client.access" = "allowed"; }
+            ];
+          };
+          "context.exec" = [
+            { path = "${pkgs.pulseaudio}/bin/pactl"; args = "load-module module-always-sink"; }
+            { path = "${pkgs.pulseaudio}/bin/pactl"; args = "load-module module-zeroconf-publish"; }
+          ];
+        };
+
+      "pipewire/pipewire.conf.d/zeroconf.conf".source = json.generate "zeroconf.conf"
+        {
+          "context.modules" = [
+            { name = "libpipewire-module-zeroconf-discover"; args = { }; }
+          ];
+        };
+
+      "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+        bluez_monitor.properties = {
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = true,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+        }
+      '';
+    };
 }
+
