89e01fa738622424b15c98edb01fe692b5d8e801
Mathias Rüdiger <ruediger@blueboot.org>
2024-03-11 10:55:14 +0100

    rename kubectl alias

--- modules/justwatch.nix
+++ modules/justwatch.nix
@@ -15,7 +15,7 @@
   programs.bash.shellAliases = {
     ks = "kubectl --context gke_justwatch-compute_europe-west1-b_jw-k8s-stage-eu-1";
     kp = "kubectl --context gke_justwatch-compute_europe-west1-d_jw-k8s-prod-eu-1";
-    ksn = "kubectl --context gke_justwatch-compute_europe-west1-b_jw-k8s-stage-eu-2";
+    kn = "kubectl --context gke_justwatch-compute_europe-west1-b_jw-k8s-stage-eu-2";
     tgs = "TF_VAR_env=stage terragrunt";
     tgp = "TF_VAR_env=prod terragrunt";
     tfs = "TF_VAR_env=stage terraform";
@@ -26,6 +26,7 @@
     source ${pkgs.complete-alias}/bin/complete_alias
     complete -F _complete_alias kp
     complete -F _complete_alias ks
+    complete -F _complete_alias kn
     complete -F _complete_alias tgs
     complete -F _complete_alias tgp
   '';
