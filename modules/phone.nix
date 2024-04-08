ac9ebf2a04084bc5d86bead9102c6e232ba23733
Mathias Rüdiger <ruediger@blueboot.org>
2024-04-08 09:34:20 +0200

    add ausweisapp

--- modules/phone.nix
+++ modules/phone.nix
@@ -2,8 +2,11 @@
 {
   environment.systemPackages = with pkgs; [
     go-mtpfs
+    ausweisapp
   ];
 
   programs.adb.enable = true;
   users.users.bag.extraGroups = [ "adbusers" ];
+
+  networking.firewall.allowedUDPPorts = [ 24727 ];
 }
