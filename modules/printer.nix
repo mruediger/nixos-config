6a72777ebd0cc434a1ab101432ec348c0a8236d5
Mathias Rüdiger <ruediger@blueboot.org>
2024-04-15 14:11:32 +0200

    add Brother printer

--- modules/printer.nix
+++ modules/printer.nix
@@ -3,7 +3,7 @@
   services.printing = {
     enable = true;
     startWhenNeeded = true;
-    drivers = with pkgs; [ gutenprint hplip ];
+    drivers = with pkgs; [ gutenprint hplip brlaser ];
   };
 
   services.avahi = {
@@ -12,20 +12,31 @@
   };
 
   hardware.printers = {
-    ensurePrinters = [{
-      name = "HP_LaserJet_Pro_M148dw";
-      ppdOptions = {
-        PageSize = "A4";
-        Duplex = "DuplexNoTumble";
-      };
-      # lpinfo -v  or   hp-setup -i  or   hp-makeuri <IP>
-      deviceUri = "hp:/net/HP_LaserJet_Pro_M148-M149";
+    ensurePrinters = [
+      {
+        name = "HP_LaserJet_Pro_M148dw";
+        ppdOptions = {
+          PageSize = "A4";
+          Duplex = "DuplexNoTumble";
+        };
+        # lpinfo -v  or   hp-setup -i  or   hp-makeuri <IP>
+        deviceUri = "hp:/net/HP_LaserJet_Pro_M148-M149";
 
-      # lpinfo -m
-      model = "HP/hp-laserjet_pro_m148-m149-ps.ppd.gz";
-      description = "HP LaserJet Pro M148DW";
-      location = "Wohnzimmer";
-    }];
+        # lpinfo -m
+        model = "HP/hp-laserjet_pro_m148-m149-ps.ppd.gz";
+        description = "HP LaserJet Pro M148DW";
+        location = "Wohnzimmer";
+      }
+      {
+        name = "Brother_DCP-L2510";
+        deviceUri = "usb://Brother/DCP-L2510D%20series?serial=E78274E1N234831";
+        ppdOptions = {
+          PageSize = "A4";
+        };
+        model = "drv:///brlaser.drv/brl2500d.ppd";
+      }
+    ];
+
     ensureDefaultPrinter = "HP_LaserJet_Pro_M148dw";
   };
 
