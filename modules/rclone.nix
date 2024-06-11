dd6fcc8f016ea1a98ddc6d39e5651109ca49ada6
Mathias Rüdiger <ruediger@blueboot.org>
2024-06-11 10:29:29 +0200

    remove rclone mkSyncService and mkSyncTimer

--- modules/rclone.nix
+++ modules/rclone.nix
@@ -20,29 +20,6 @@
               WantedBy = [ "default.target" ];
             };
           };
-
-        mkSyncService = path1: path2: {
-          Service = {
-            Type = "simple";
-            Environment = "PATH=/run/wrappers/bin";
-            ExecStart = ''
-              ${pkgs.rclone}/bin/rclone bisync ${path1} ${path2} --exclude ".git/**"
-            '';
-          };
-        };
-
-        mkSyncTimer = unit: {
-          Timer = {
-            # 5 minutes after boot
-            OnBootSec = "5m";
-            # 5 minutes after last finished
-            OnUnitInactiveSec = "5m";
-            # run once when the timer is started
-            Unit = "${unit}";
-          };
-          Install = { WantedBy = [ "timers.target" ]; };
-        };
-
         services = [
             "nextcloud_blueboot"
             "nextcloud_darksystem"
