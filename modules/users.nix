94ab9b9b26005eccc40d9e96eead315e68262b59
Mathias Rüdiger <ruediger@blueboot.org>
2024-06-11 10:54:18 +0200

    update authorized ssh-keys

--- modules/users.nix
+++ modules/users.nix
@@ -8,7 +8,8 @@
     extraGroups = [ "wheel" "networkmanager" "input" "video" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsvZoC8Cl2by+hHJWhZYPLKGfM50Y6OgUXehSwFGVt1 bag@josephine"
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC0mY0TcE84d7TK5vHt8Fcekm8QBcMN0kA+WT+ED5XF3s87wIQGic/BUteT6At4cL728v7lzdHPTTqYqKLK9Q2vLfz31JhbVWcWwBptSJCjCuAnf7fN9QP+NJ3Bi+/MAlX8wQ7raRhmO2l2u6yL8SE9Ig4BPYwUf60dY97b1t3RXZEUYu1GR0lxpzjCJeER+c9JwczzkegwWa+NmajK2hVOBrbj+BXLiJOTUHVk0r7HU3hkt5YatTQSWAVrP/SY0pcrRidy5Eslyi/cszI6C4DApXAHH+vC6EdBjKL/B3IgujgU0GtVF5ec/Ta3zO09MO0YbKDUDMnRJJV9ruJnyVmCEYCd73E9FklL9Ki1zIPotsyTd/8+5gcoz+p7sNGV1NqtMOH8EEd5OpkDHr1Eyf0qUcZBS5sRb2dbf6p9plv6UuynXMfI+ldYXjB4nLOk3CnNM5RLDFZB4c7HTgp6dGnlBU14tnZ/s7e/Ph3DU/hUkWJ9a44+GEf529vjkMcKocM= bag@butterfly"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL+DjDnJkX9xDJAY6ciUUxO2W0413w1Bvr8oQU2ufpNs bag@farting-unicorn"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPgc+zDAB4Y1HcvewdiKdKlcRo2wf4iO5aWIYab7EroV bag@butterfly"
     ];
   };
 
