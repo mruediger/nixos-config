baaa0aa9459fd2582f9863d4d18caf3833234763
Mathias Rüdiger <ruediger@blueboot.org>
2024-06-12 15:16:38 +0200

    update ssh keys

--- modules/users.nix
+++ modules/users.nix
@@ -8,7 +8,7 @@
     extraGroups = [ "wheel" "networkmanager" "input" "video" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsvZoC8Cl2by+hHJWhZYPLKGfM50Y6OgUXehSwFGVt1 bag@josephine"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL+DjDnJkX9xDJAY6ciUUxO2W0413w1Bvr8oQU2ufpNs bag@farting-unicorn"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJFZoyPLGIlSzI5zA8MLvLM3P2rEUPAtgXYFhv0Y+8Nd bag@farting-unicorn"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPgc+zDAB4Y1HcvewdiKdKlcRo2wf4iO5aWIYab7EroV bag@butterfly"
     ];
   };
