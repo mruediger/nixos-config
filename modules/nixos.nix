f62e81751fbcda9d849f03cdb918306bc9e8ff7c
Mathias Rüdiger <ruediger@blueboot.org>
2025-01-16 10:38:50 +0100

    add nix-community.cachix.org cache settings

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -7,6 +7,14 @@
   nix = {
     settings.auto-optimise-store = true;
 
+    settings.trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+
+    settings.substituters = [
+      "https://nix-community.cachix.org"
+    ];
+
     package = pkgs.nixVersions.stable;
     extraOptions = ''
       experimental-features = nix-command flakes
