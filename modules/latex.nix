9d953af837b2eea5959025db0ed111769d48fb49
Mathias Rüdiger <ruediger@blueboot.org>
2025-02-04 14:19:51 +0100

    update latex

--- modules/latex.nix
+++ modules/latex.nix
@@ -5,6 +5,21 @@
       ps: with ps; [
         marvosym
         droid
+        xurl
+        enumitem
+        sectsty
+        xstring
+        xifthen
+        ifmtarg
+        lualatex-math
+        roboto
+        fontawesome5
+        sourcesanspro
+        tcolorbox
+        environ
+        tikzfill
+        wrapfig
+        capt-of
       ]
     ))
     pandoc
