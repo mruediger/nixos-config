39e59c08c5634afe1b2604fd7991623ac995636b
Mathias Rüdiger <ruediger@blueboot.org>
2025-02-28 10:45:09 +0100

    add webdav bisync for nextcloud mounts

--- modules/rclone.nix
+++ modules/rclone.nix
@@ -7,7 +7,7 @@
   home-manager.sharedModules = [
     ({ lib, pkgs, config, ... }:
       let
-        mkRcloneService = environment:
+        mkRcloneMountService = environment:
           lib.nameValuePair "rclone-${environment}" {
             Service = {
               Type = "simple";
@@ -30,9 +30,44 @@
             "dropbox"
         ];
         directories = map (v: "${config.home.homeDirectory}/rclone/${v}") services;
+
+        mkRcloneBiSyncService = remote: remote_path: local_path: {
+          Unit = {
+            Description = "RClone bisync: ${remote}:${remote_path} ${local_path}";
+            After = "network-online.target";
+          };
+          Service = {
+            Type = "simple";
+            ExecStart = ''
+              ${pkgs.rclone}/bin/rclone bisync ${remote}:${remote_path} ${local_path} --compare size,modtime
+            '';
+          };
+          Install.WantedBy = ["multi-user.target"];
+        };
+
+        mkTimer = {
+          Unit.Description = "RClone bisync timer";
+          Timer = {
+            OnBootSec = "5min";
+            OnUnitActiveSec = "5min";
+          };
+          Install.WantedBy = [ "multi-user.target" "timers.target" ];
+        };
       in
       {
-        systemd.user.services = builtins.listToAttrs (map mkRcloneService services) // { };
+        systemd.user = {
+          services = builtins.listToAttrs (map mkRcloneMountService services) // {
+            rc-bisync-org = mkRcloneBiSyncService "nextcloud_blueboot" "org" "${config.home.homeDirectory}/org";
+            rc-bisync-doc = mkRcloneBiSyncService "nextcloud_blueboot" "Documents" "${config.home.homeDirectory}/doc";
+            rc-bisync-books = mkRcloneBiSyncService "nextcloud_blueboot" "Books" "${config.home.homeDirectory}/media/books";
+          };
+          timers = {
+            rc-bisync-org = mkTimer;
+            rc-bisync-doc = mkTimer;
+            rc-bisync-books = mkTimer;
+          };
+          startServices = true;
+        };
         home.activation.createRcloneDirectories = let
           mkdir = (dir: ''[[ -L "${dir}" ]] || run mkdir -p $VERBOSE_ARG "${dir}"'');
         in lib.hm.dag.entryAfter [ "writeBoundary" ]
