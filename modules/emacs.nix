a4a28c536bde954fe52e4ba7188952a7b7dc9165
Mathias Rüdiger <ruediger@blueboot.org>
2025-04-01 18:06:36 +0200

    add packages to emacs via nix

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -4,6 +4,46 @@
   environment.systemPackages = with pkgs; [
     ((emacsPackagesFor emacs-version).emacsWithPackages (epkgs: with epkgs; [
       treesit-grammars.with-all-grammars
+      ag
+      company
+      consult
+      copy-as-format
+      csv-mode
+      diff-hl
+      direnv
+      doom-modeline
+      eglot-java
+      elysium
+      flycheck
+      forge
+      gcmh
+      git-link
+      gptel
+      gruvbox-theme
+      highlight-indent-guides
+      jsonnet-mode
+      ledger-mode
+      magit
+      magit-todos
+      marginalia
+      nerd-icons-dired
+      nix-mode
+      nov
+      ob-async
+      orderless
+      org-tree-slide
+      pass
+      pdf-tools
+      rego-mode
+      sudo-edit
+      terraform-mode
+      typst-ts-mode
+      use-package-chords
+      vertico
+      which-key
+      xkcd
+      yasnippet
+      yasnippet-snippets
     ]))
 
     silver-searcher # for ag.el
