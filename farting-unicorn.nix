77fddd8f40a0810b56508d01fd246b31e9b25189
Mathias Rüdiger <ruediger@blueboot.org>
2025-04-25 21:23:25 +0200

    fix swap and add powerkey

--- farting-unicorn.nix
+++ farting-unicorn.nix
@@ -31,8 +31,10 @@
     fsType = "ext4";
   };
 
-  boot.initrd.luks.devices."luks-47e2e76d-6ca1-4580-a646-4276cbbebd2b".device =
-    "/dev/disk/by-uuid/47e2e76d-6ca1-4580-a646-4276cbbebd2b";
+  boot.initrd.luks.devices = {
+    "root".device = "/dev/disk/by-uuid/47e2e76d-6ca1-4580-a646-4276cbbebd2b";
+    "swap".device = "/dev/disk/by-uuid/c893cad7-f94c-4fc0-b044-9889e79c07ad";
+  };
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/621A-C573";
@@ -70,5 +72,11 @@
         }
       ];
     };
+  };
+
+  services.logind = {
+    extraConfig = ''
+      HandlePowerKey=hibernate
+    '';
   };
 }
