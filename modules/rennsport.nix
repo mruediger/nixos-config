509ac4bcb983086bc518fb52b107eb3a96959e0b
Mathias Rüdiger <ruediger@blueboot.org>
2025-07-08 09:52:39 +0200

    add go env variables for rennsport

--- modules/rennsport.nix
+++ modules/rennsport.nix
@@ -5,4 +5,9 @@
     enable = true;
   };
 
+  environment.variables = {
+    GONOPROXY ="gitlab.com/rennsport/*";
+    GONOSUMDB="gitlab.com/rennsport/*";
+    GOPRIVATE="gitlab.com/rennsport/*";
+  };
 }
