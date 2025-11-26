0d7c50afb523cfb44f057626c4cc3eded4c199e0
Mathias Rüdiger <ruediger@blueboot.org>
2025-11-26 08:35:42 +0100

    add gamemode

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -20,6 +20,26 @@
     extraCompatPackages = [ pkgs.proton-ge-bin ];
   };
 
+  programs.gamemode = {
+    enable = true;
+    settings = {
+      general = {
+        renice = 10;
+      };
+
+      # Warning: GPU optimisations have the potential to damage hardware
+      gpu = {
+        apply_gpu_optimisations = "accept-responsibility";
+        gpu_device = 0;
+        amd_performance_level = "high";
+      };
+
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+        end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+      };
+    };
+  };
   hardware = {
     graphics = {
       enable = true;
