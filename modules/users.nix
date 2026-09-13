30b567318776e74da99cf8a8667f6a8d81d1c8c1
Mathias Rüdiger <ruediger@blueboot.org>
2026-09-13 18:25:21 +0200

    add bag to dialout to flash the flow3r

--- modules/users.nix
+++ modules/users.nix
@@ -5,7 +5,7 @@
     home = "/home/bag";
     isNormalUser = true;
     uid = 1000;
-    extraGroups = [ "wheel" "networkmanager" "input" "video" ];
+    extraGroups = [ "wheel" "networkmanager" "input" "video" "dialout" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB09ouGKRRbYP5ZpZ5wYgfKHnopafi+ppzQn+3AMwq4t bag@josephine"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJFZoyPLGIlSzI5zA8MLvLM3P2rEUPAtgXYFhv0Y+8Nd bag@farting-unicorn"
